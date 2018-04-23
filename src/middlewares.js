f8af6b58fcb9217c8bd5e767c64cd3c624e3f5d2
Ice <ice.zjchen@gmail.com>
2018-04-23 16:49:05 +0800

    support `once`, `cocurrent`, `selectQuerySet`

--- src/middlewares.js
+++ src/middlewares.js
@@ -17,14 +17,25 @@ const createApiActionErrorMeta = (params, error) => ({
   error: { message: error.message, ...error },
 });
 
+const getQuery = (state, selectQuerySet, paramsKey) => {
+  const querySet = selectQuerySet(state);
+
+  return querySet ? querySet[paramsKey] : null;
+};
+
 function callAPIMiddleware(opts) {
   const apiDataPath = opts ? opts.apiDataPath : null;
+  const cache = new Map();
+
   return ({ dispatch, getState }) => next => (action) => {
     const { type, payload = {} } = action;
     const {
       async = false,
       callAPI,
       shouldCallAPI = () => true,
+      once = false,
+      selectQuerySet = () => null,
+      concurrent = false,
       computeParams = params => params,
       withTableUpdate = {},
     } = action.meta || {};
@@ -42,8 +53,27 @@ function callAPIMiddleware(opts) {
       throw new Error('Expected callAPI to be a function.');
     }
 
+    // deprecated
     if (!shouldCallAPI(getState(), action)) {
       return next(action);
+    }
+
+    const computedPayload = computeParams(payload);
+    const paramsKey = JSON.stringify(computedPayload);
+
+    const availableData = once && _.get(getQuery(getState(), selectQuerySet, paramsKey), 'response.data', null);
+    if (availableData) {
+      return Promise.resolve(availableData);
+    }
+
+    const requestKey = `${type}--${paramsKey}`;
+
+    if (!concurrent) {
+      const cachedPromise = cache.get(requestKey);
+
+      if (cachedPromise) {
+        return cachedPromise;
+      }
     }
 
     const requestType = `${type}_${REQUEST}`;
@@ -52,41 +82,58 @@ function callAPIMiddleware(opts) {
 
     dispatch(Object.assign({}, {
       type: requestType,
-      payload: computeParams(payload),
+      payload: computedPayload,
     }));
 
-    return callAPI(payload)
-      .then((response) => {
-        const res = !apiDataPath ? response : _.get(response, apiDataPath);
+    const removeCachedPromise = () => {
+      if (!concurrent) {
+        cache.delete(requestKey);
+      }
+    };
 
-        if (_.has(withTableUpdate, 'tableName') && _.has(withTableUpdate, 'selectEntities')) {
-          dispatch(Object.assign({}, {
-            type: UPDATE_ENTITY_TABLE,
-            payload: {
-              tableName: withTableUpdate.tableName,
-              entities: withTableUpdate.selectEntities(res),
-            },
-          }));
-        }
+    const handleResult = (response) => {
+      removeCachedPromise();
+      const res = !apiDataPath ? response : _.get(response, apiDataPath);
 
-        return dispatch(Object.assign({}, {
-          type: successType,
-          payload: res,
-          meta: createApiActionMeta(
-            computeParams(payload),
-            res,
-          ),
-        }));
-      })
-      .catch((error) => {
+      if (_.has(withTableUpdate, 'tableName') && _.has(withTableUpdate, 'selectEntities')) {
         dispatch(Object.assign({}, {
-          type: failureType,
-          error,
-          meta: createApiActionErrorMeta(computeParams(payload), error),
+          type: UPDATE_ENTITY_TABLE,
+          payload: {
+            tableName: withTableUpdate.tableName,
+            entities: withTableUpdate.selectEntities(res),
+          },
         }));
+      }
 
-        throw error;
-      });
+      return dispatch(Object.assign({}, {
+        type: successType,
+        payload: res,
+        meta: createApiActionMeta(
+          computedPayload,
+          res,
+        ),
+      }));
+    };
+
+    const handleError = (error) => {
+      dispatch(Object.assign({}, {
+        type: failureType,
+        error,
+        meta: createApiActionErrorMeta(computedPayload, error),
+      }));
+
+      throw error;
+    };
+
+    const promise = callAPI(payload)
+      .then(handleResult)
+      .catch(handleError);
+
+    if (!concurrent) {
+      cache.set(requestKey, promise);
+    }
+
+    return promise;
   };
 }
 
